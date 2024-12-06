aae9e07bce5c9cd68727c9bb0b91fc23111b8ade
Angela-G9 <angelagathoni9568@gmail.com>
2024-12-06 20:01:37 +0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+//const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+// Code your solution here 
+// Use reduce to calculate the total number of batteries
+//const totalBatteries = batteryBatches.reduce(total,batch) = total + batch, 0);
+
+
+//console.log(batteryBatches);
+
+
+
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+// Use reduce to calculate the total number of batteries
+//const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+//console.log(totalBatteries); // Should output the sum of all batteries
 
 // Code your solution here
+
+
+const batteryBatches = [4,5,3,4,4,6,5];  
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+console.log(totalBatteries); 
